cb85dbd659995635985526a1cabcca51947cbbdd
Bhuwana <97886708+Bhuwana-Github@users.noreply.github.com>
2022-05-29 19:08:20 +0100

    Add files via upload
    practice-format_program

---
+++ examp_format.java
@@ -0,0 +1,11 @@
+package Basic_Programs;
+import sun.jvm.hotspot.oops.java_lang_Class;.lang.
+public class examp_format {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner 
+
+	}
+
+}
